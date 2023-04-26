55d1f1d40204cc4bfe02a70207627cfd4ccfef97
SimoCrash <simone.crescenzi@hotmail.com>
2023-04-26 17:29:27 +0200

    inserito Update, totalmente funzionante

--- Controllers/PhotoController.cs
+++ Controllers/PhotoController.cs
@@ -26,7 +26,7 @@ namespace net_il_mio_fotoalbum.Controllers
             using var ctx = new PhotoContext();
             var formModel = new PhotoFormModel
             {
-                Categories = ctx.Categories.Select(i => new SelectListItem(i.Name, i.Id.ToString())).ToArray(),
+                Categories = ctx.Categories.Select(c => new SelectListItem(c.Name, c.Id.ToString())).ToArray(),
             };
 
             return View(formModel);
@@ -40,7 +40,7 @@ namespace net_il_mio_fotoalbum.Controllers
 
             if (!ModelState.IsValid)
             {
-                form.Categories = ctx.Categories.Select(i => new SelectListItem(i.Name, i.Id.ToString())).ToArray();
+                form.Categories = ctx.Categories.Select(c => new SelectListItem(c.Name, c.Id.ToString())).ToArray();
             }
 
             form.Photo.Categories = form.SelectedCategories.Select(sc => ctx.Categories.First(c => c.Id == Convert.ToInt32(sc))).ToList();
@@ -51,6 +51,59 @@ namespace net_il_mio_fotoalbum.Controllers
             return RedirectToAction("Index");
         }
         
+        public IActionResult Update(int id)
+        {
+            using var ctx = new PhotoContext();
+
+            var photo = ctx.Photos.Include(p => p.Categories).SingleOrDefault(p => p.Id == id);
+
+            if (photo == null)
+            {
+                return View($"Non è stato trovato l'id {id}");
+            }
+
+            var formModel = new PhotoFormModel
+            {
+                Photo = photo,
+                Categories = ctx.Categories.ToArray().Select(c => new SelectListItem(c.Name, c.Id.ToString(), photo.Categories!.Any(_c => _c.Id == c.Id))).ToArray(),
+            };
+            
+            formModel.SelectedCategories = formModel.Categories.Where(sc => sc.Selected).Select(sc => sc.Value).ToList();
+
+            return View(formModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(int id, PhotoFormModel form)
+        {
+            using var ctx = new PhotoContext();
+
+            if (!ModelState.IsValid)
+            {
+                form.Categories = ctx.Categories.Select(c => new SelectListItem(c.Name, c.Id.ToString())).ToArray();
+
+                return View(form);
+            }
+
+            var PhotoToUpdate = ctx.Photos.Include(p => p.Categories).SingleOrDefault(i => i.Id == id);
+
+            if (PhotoToUpdate == null)
+            {
+                return View($"Non è stato trovato l'id {id}");
+            }
+
+            PhotoToUpdate.Title = form.Photo.Title;
+            PhotoToUpdate.Description = form.Photo.Description;
+            PhotoToUpdate.ImageUrl = form.Photo.ImageUrl;
+            PhotoToUpdate.Visible = form.Photo.Visible;
+            PhotoToUpdate.CategoriesId = form.Photo.CategoriesId;
+            PhotoToUpdate.Categories = form.SelectedCategories.Select(sc => ctx.Categories.First(c => c.Id == Convert.ToInt32(sc))).ToList();
+
+            ctx.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
 
         public IActionResult Privacy()
         {
